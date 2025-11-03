fd758d91ae0ee5e314ff4f9eaef35265b12a762e
Kristian <118914991+Krikoman17@users.noreply.github.com.>
2025-11-03 15:53:32 +0100

    PP-81 Minor fixes

--- web/src/lib/errorResponseHandler.ts
+++ web/src/lib/errorResponseHandler.ts
@@ -7,17 +7,10 @@ export const errorResponseHandler = async (error: Error, intl: IntlShape) => {
     if (error instanceof HTTPError) {
         try {
             const data = await error.response.json();
-            console.log(1);
             message = intl.formatMessage({ id: data.title });
         } catch {
-            console.log(2);
-
             message = error.message;
         }
-    } else {
-        console.log(3);
-
-        message = error.message;
     }
     toast.error(message);
 };
