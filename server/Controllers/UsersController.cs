fdc80c440e06b0565b286c22061a30b3b01f2ade
Kristian <118914991+Krikoman17@users.noreply.github.com.>
2025-11-12 23:51:34 +0100

    change password backend

--- server/Controllers/UsersController.cs
+++ server/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Asp.Versioning;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using server.Foundation.Result;
+using server.Models.Auth;
 using server.Models.User;
 using server.Services;
 
@@ -12,7 +13,7 @@ namespace server.Controllers;
 [ApiVersion(1)]
 [Route("api/v{version:apiVersion}/[controller]")]
 [Authorize]
-public class UsersController(IUserService userService) : ControllerBase
+public class UsersController(IUserService userService, IAuthService authService) : ControllerBase
 {
     [HttpGet("me/personal-information")]
     public async Task<ActionResult<PersonalInformationResDto>> GetPersonalInformation()
@@ -45,6 +46,26 @@ public class UsersController(IUserService userService) : ControllerBase
         }
 
         var result = await userService.UpdatePersonalInformationAsync(parsedUserId, request);
+
+        if (result.IsFailure)
+        {
+            return result.ToProblemDetails();
+        }
+
+        return NoContent();
+    }
+
+    [HttpPost("me/change-password")]
+    public async Task<ActionResult> ChangePassword(ChangePasswordReqDto request)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (userId is null || !Guid.TryParse(userId, out var parsedUserId))
+        {
+            return Unauthorized();
+        }
+
+        var result = await authService.ChangePasswordAsync(parsedUserId, request);
 
         if (result.IsFailure)
         {
--- server/Services/IAuthService.cs
+++ server/Services/IAuthService.cs
@@ -10,9 +10,10 @@ public interface IAuthService
 {
     Task<Result<UserResDto>> RegisterStudentAsync(StudentRegisterReqDto request);
     Task<Result<UserResDto>> RegisterInternshipHandlerAsync(InternshipHandlerRegisterReqDto request);
-    Task<Result<UserResDto>> GetUserByIdAsync(Guid userId);
     Task<Result<TokenResDto>> LoginAsync(LoginReqDto request);
     Task<Result<TokenResDto>> RefreshTokensAsync(RefreshTokenReqDto request);
     Task<Result<TokenResDto>> ChangeDefaultPassword(ChangeDefaultPasswordReqDto request);
-    Task<Result> LogoutAsync(string userId);
+    Task<Result> LogoutAsync(string accessToken);
+    Task<Result<UserResDto>> GetUserByIdAsync(Guid userId);
+    Task<Result> ChangePasswordAsync(Guid userId, ChangePasswordReqDto request);
 }
